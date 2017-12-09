928888dbc2f9b935888e8d1635c0a70073b088a1
chenyu36 <chenyu36@gmail.com>
2017-12-09 17:55:02 -0500

    Adjusted the angle parameter due to the change in phone mount's location

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CargoBotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CargoBotConstants.java
@@ -53,7 +53,7 @@ public interface CargoBotConstants {
 
     double RED_TURN_ANGLE = -35;
     double BLUE_TURN_ANGLE = 60;
-    double PHONE_MOUNT_V2_ANGLE = 5; // since phone mount is at an angle in Curious George V2
+    double PHONE_MOUNT_V2_ANGLE = -20; // since phone mount is at an angle in Curious George V2
     double RED_TURN_ANGLE_V2 = RED_TURN_ANGLE + PHONE_MOUNT_V2_ANGLE;
     double BLUE_TURN_ANGLE_V2 = BLUE_TURN_ANGLE + PHONE_MOUNT_V2_ANGLE;
 
