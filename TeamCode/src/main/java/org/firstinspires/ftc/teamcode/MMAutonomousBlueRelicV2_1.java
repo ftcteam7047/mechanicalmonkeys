41afdc81c8af93b5b9a9666f2f6b91c53640530c
chenyu36 <chenyu36@gmail.com>
2017-12-12 12:03:35 -0500

    Disabled the test program (blue relic v2_1) that rotate the robot on the platform to read VuMark. (good to read VuMark, but much less reliable for ball detection.)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MMAutonomousBlueRelicV2_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MMAutonomousBlueRelicV2_1.java
@@ -38,6 +38,7 @@ import android.util.Log;
 import com.kauailabs.navx.ftc.AHRS;
 import com.kauailabs.navx.ftc.navXPIDController;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -96,7 +97,7 @@ import static java.lang.StrictMath.sin;
  */
 
 @Autonomous(name="Blue Relic V2_1", group ="Blue")
-//@Disabled
+@Disabled
 public class MMAutonomousBlueRelicV2_1 extends LinearOpMode {
 
     public static final String TAG = "Vuforia VuMark Sample";
